91d14432734fe652263917e8ad0e11831f05174d
Eduardo Garcia Misiuk <eduardogmisiuk@gmail.com>
2019-09-23 10:18:12 -0300

    Remove unused endpoints

--- verne-webhooks/routes/verneRoute.ts
+++ verne-webhooks/routes/verneRoute.ts
@@ -1,7 +1,7 @@
 import { logger } from "@dojot/dojot-module-logger";
 import express from "express";
 import util from "util";
-import * as utils from "../utils/utils";
+import ProjectUtils from "../utils/utils";
 
 const TAG = { filename: "verneRoute" };
 
@@ -10,16 +10,6 @@ const TAG = { filename: "verneRoute" };
  * @param app Express application to be used
  */
 const verneRoute = (app: express.Application) => {
-  /**
-   * Endpoint for webhook on_register
-   */
-  app.post("/reg", (req: express.Request, res: express.Response) => {
-    logger.debug("Received request in /reg", TAG);
-    logger.debug(`Request body:\n${util.inspect(req.body)}`, TAG);
-
-    res.status(200).send({});
-  });
-
   /**
    * Endpoint for webhook on_publish
    */
@@ -33,39 +23,10 @@ const verneRoute = (app: express.Application) => {
     res.status(200).send({});
   });
 
-  /**
-   * Endpoint for webhook on_subscribe
-   */
-  app.post("/sub", (req: express.Request, res: express.Response) => {
-    logger.debug("Received request in /sub", TAG);
-    logger.debug(`Request body:\n${util.inspect(req.body)}`, TAG);
 
-    res.status(200).send({});
-  });
 
-  /**
-   * Endpoint for webhook on_unsubscribe
-   */
-  app.post("/unsub", (req: express.Request, res: express.Response) => {
-    logger.debug("Received request in /unsub", TAG);
-    logger.debug(`Request body:\n${util.inspect(req.body)}`, TAG);
 
-    res.status(200).send({
-      result: "ok",
-      topics: req.body.topic,
-    });
-  });
 
-  /**
-   * Endpoint for webhook on_deliver
-   */
-  app.post("/deliver", (req: express.Request, res: express.Response) => {
-    logger.debug("Received request in /deliver", TAG);
-    logger.debug(`Request body:\n${util.inspect(req.body)}`, TAG);
-
-    res.status(200).send({
-      result: "ok",
-    });
   });
 };
 
