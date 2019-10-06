f32c9e5a1ba879ae04e30cf03ac9717732469c59
Muhamed <muhamed@cpqd.com.br>
2019-10-06 09:30:15 -0300

    Adding init call for Locust/modifying mqtt locust client call

--- locust/main.py
+++ locust/main.py
@@ -1,13 +1,48 @@
 import logging
 import os
-from locust import Locust
-from iot_device import IoT_Device
+import time
+import uuid
+
+from locust import Locust, task, TaskSet, events
+from mqtt_client import MQTT_Client
+
+from redis_client import RedisClient
+cache = RedisClient()
+cache.connect()
 
 TASK_MIN_TIME = os.environ.get("TASK_MIN_TIME", "1000")
 TASK_MAX_TIME = os.environ.get("TASK_MAX_TIME", "1000")
 
-class Client(Locust):
-    task_set = IoT_Device
 
+LOG_DIR = os.environ.get("LOCUST_LOG_DIR", '/log')
+
+if not os.path.exists(LOG_DIR):
+    os.makedirs(LOG_DIR)
+
+filename_dir = "{0}/{1}.log".format(LOG_DIR, uuid.uuid4())
+
+
+class MqttLocust(Locust):
+    def __init__(self, *args, **kwargs):
+        super(Locust, self).__init__(*args, **kwargs)
+        device_id = cache.next_device_id()
+        self.client = MQTT_Client(device_id, filename_dir)
+        self.client.connect()
+
+
+class ThingBehavior(TaskSet):
+    @task
+    def publish(self):
+        self.client.publishing()
+
+    def on_start(self):
+        time.sleep(5)
+
+    def on_stop(self):
+        self.client.save_log_list()
+
+
+class Client(MqttLocust):
+    task_set = ThingBehavior
     min_wait = int(TASK_MIN_TIME)
     max_wait = int(TASK_MAX_TIME)
