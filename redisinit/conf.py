87963d03cfd33fcdc6b1a215f880edb67ddb4b08
Mariane Previde <mprevide@cpqd.com.br>
2019-10-09 14:32:18 -0300

    change config

--- redisinit/conf.py
+++ redisinit/conf.py
@@ -5,5 +5,5 @@ redis_port = os.environ.get("REDIS_PORT","6379")
 
 ejbca_url = os.environ.get("EJBCA_URL","http://172.17.0.2:5583") 
 ejbca_CAName = "IOTmidCA"
-dns_cert = ["localhost"]
+dns_cert = []
 
