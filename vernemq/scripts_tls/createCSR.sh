9f160c4ab8428216f768771fc0fd6a23b730a840
Mariane Previde <mprevide@cpqd.com.br>
2019-10-29 12:57:50 -0300

    dns

--- vernemq/scripts_tls/createCSR.sh
+++ vernemq/scripts_tls/createCSR.sh
@@ -8,7 +8,9 @@ BASE_DIR=${BASE_DIR:-"/vernemq"}
 
 . ${BASE_DIR}/scripts_tls/_initVariables.sh
 
-echo "Create CSR for ${certCname}"
-openssl req -new  -sha256 -out ${certDir}/${certCsrFile} -key ${certDir}/${certKeyFile} \
-        --subj "/CN=${certCname}"
-
+ echo "Create CSR for ${certCname}"
+ openssl req -new  -sha256 -out ${certDir}/${certCsrFile} -key ${certDir}/${certKeyFile} \
+            -addext "subjectAltName = DNS:${certDns}" \
+	        -addext "keyUsage = Digital Signature, Non Repudiation, Key Encipherment" \
+            -addext "basicConstraints  =  CA:FALSE" \
+            --subj "/CN=${certCname}"
