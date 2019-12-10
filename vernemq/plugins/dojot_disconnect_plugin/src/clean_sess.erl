a2606bcf0ea5c8f1aa91b8d64db018fbde30d082
Muhamed <muhamed@cpqd.com.br>
2019-12-10 10:52:25 -0300

    fix:only devices will set timeout

--- vernemq/plugins/dojot_disconnect_plugin/src/clean_sess.erl
+++ vernemq/plugins/dojot_disconnect_plugin/src/clean_sess.erl
@@ -4,8 +4,22 @@
 
 -export([
     set_connection_timeout/1,
-    disconnect_client/1
+    disconnect_client/1,
+    is_dojot_user/1
 ]).
+
+is_dojot_user(Username) ->
+
+    % we will split the username. If dojot username is tenant:deviceid
+    % the length of list will aways 2. If not, it will not be dojot user.
+    Res = binary:split(Username, <<":">>, [global]),
+    case length(Res) of
+        2 ->
+            is_user;
+        _->
+            not_user
+            
+    end.
 
 disconnect_client(SubId) ->
     error_logger:info_msg("disconnect_client: ~p ~n", [SubId]),
--- vernemq/plugins/dojot_disconnect_plugin/src/dojot_disconnect_plugin.erl
+++ vernemq/plugins/dojot_disconnect_plugin/src/dojot_disconnect_plugin.erl
@@ -9,8 +9,16 @@
          auth_on_subscribe/3]).
 
 auth_on_register({_IpAddr, _Port} = Peer, {_MountPoint, _ClientId} = SubscriberId, UserName, Password, CleanSession) ->
-    ok = clean_sess:set_connection_timeout(SubscriberId),
-    ok.
+    
+    case clean_sess:is_dojot_user(UserName) of
+        is_user ->
+            ok = clean_sess:set_connection_timeout(SubscriberId),
+            ok;
+        not_user ->
+            ok
+    end.
+
+
 
 auth_on_publish(UserName, {_MountPoint, _ClientId} = SubscriberId, QoS, Topic, Payload, IsRetain) ->
     next.
