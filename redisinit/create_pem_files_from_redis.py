8419c74a73bb2aed684afd0a4884c35f1d851f20
Eduardo Garcia Misiuk <eduardogmisiuk@gmail.com>
2019-12-18 14:42:05 -0300

    Add error handling in create_pem_files_from_redis script

--- redisinit/create_pem_files_from_redis.py
+++ redisinit/create_pem_files_from_redis.py
@@ -2,6 +2,7 @@
 import redis
 import conf
 import logging
+import sys
 from thing import Thing
 
 
@@ -10,23 +11,37 @@ if __name__ == "__main__":
     format = "%(asctime)s: %(message)s"
     logging.basicConfig(format=format, level=logging.DEBUG, datefmt="%H:%M:%S")
 
-    r = redis.Redis(host = conf.redis_host, port = conf.redis_port, db = 0)
 
-    i = 0
-    print ("PEM Keys:")
-    for key in r.scan_iter(count=2):
-        x = (r.hmget(name=key, keys="private_key"))
-        i  = i + 1
-        print (i)
-        with open("cert_things/"+(key.decode("utf-8"))+".key", "w") as keyFile:
-            keyFile.write((x[0]).decode("utf-8") )
+    try:
+        r = redis.Redis(host = conf.redis_host, port = conf.redis_port, db = 0)
+        r.ping()
+    except Exception as exception:
+        logging.error("Error while connecting to Redis.")
+        sys.exit(1)
 
-    i = 0
-    print ("PEM Crts:")
-    for key in r.scan_iter(count=2):
-        x = (r.hmget(name=key, keys="thing_certificate"))
-        i  = i + 1
-        print (i)
-        with open("cert_things/"+(key.decode("utf-8"))+".crt", "w") as crtFile:
-            crtFile.write((x[0]).decode("utf-8") )
+
+    try:
+        logging.info("Creating key files...")
+        for key in r.scan_iter(count=2):
+            x = (r.hmget(name=key, keys="private_key"))
+            with open("cert/"+(key.decode("utf-8"))+".key", "w") as keyFile:
+                keyFile.write((x[0]).decode("utf-8") )
+    except Exception as exception:
+        logging.error("Error while saving key files: %s", str(exception))
+        sys.exit(1)
+    else:
+        logging.info("Successfully created!")
+
+
+    try:
+        logging.info("Creating certificate files...")
+        for key in r.scan_iter(count=2):
+            x = (r.hmget(name=key, keys="thing_certificate"))
+            with open("cert/"+(key.decode("utf-8"))+".crt", "w") as crtFile:
+                crtFile.write((x[0]).decode("utf-8") )
+    except Exception as exception:
+        logging.error("Error while saving key files: %s", str(exception))
+        sys.exit(1)
+    else:
+        logging.info("Successfully created!")
 
