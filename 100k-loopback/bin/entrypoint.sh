b8c139702f45cfb4ba3aa5ee838de438be7a7cc2
Jonaphael Aristil <jonaphael4@gmail.com>
2020-02-12 13:26:58 -0300

    finiched loopback script

--- 100k-loopback/bin/entrypoint.sh
+++ 100k-loopback/bin/entrypoint.sh
@@ -1,23 +1,23 @@
 #!/bin/sh
-while true; do echo hello; sleep 10;done
-
 echo "100k-loopback start"
 
 DOJOT_USERNAME=${DOJOT_USERNAME:-"admin"}
 DOJOT_PASSWORD=${DOJOT_PASSWORD:-"admin"}
-AUTH_HOST=${AUTH_HOST:-"http://10.202.46.164:30001"}
-DATA_BROKER_HOST=${DATA_BROKER_HOST:-"http://10.202.46.164:30004"}
+AUTH_HOST=${AUTH_HOST:-"http://dojot_auth_1:5000"}
+DATA_BROKER_HOST=${DATA_BROKER_HOST:-"http://dojot_data-broker_1"}
 
 JSON_CONTENT_TYPE="Content-Type:application/json"
 
 # auth
-AUTH_URL="${AUTH_HOST}/auth"
+AUTH_URL="${AUTH_HOST}"
 AUTH_DATA="{\"username\": \"${DOJOT_USERNAME}\", \"passwd\":\"${DOJOT_PASSWORD}\"}"
 DEVICE_DATA="device-data"
 DEVICE_CONFIGURE="iotagent.device"
-KAFKACAT_CONSUMER_GROUP=${KAFKACAT_CONSUMER_GROUP:-"100k-loopback-group"}
+KAFKA_HOST=${KAFKA_HOST:-"dojot_kafka_1"}
+LOOPBACK_CONSUMER_GROUP=${LOOPBACK_CONSUMER_GROUP:-"100k-loopback-group"}
 
 # device data
+TO=$(curl --silent -X POST ${AUTH_URL} -H "${JSON_CONTENT_TYPE}" -d "${AUTH_DATA}")
 TOKEN=$(curl --silent -X POST ${AUTH_URL} -H "${JSON_CONTENT_TYPE}" -d "${AUTH_DATA}" | jq '.jwt' -r)
 
 if [ ! -z "$TOKEN" ]
@@ -31,9 +31,13 @@ then
 
     if [ ! -z "${DEVICE_DATA_TOPIC}" -a ! -z "${DEVICE_CONFIGURE_TOPIC}" ]
     then
-    
-        kafkacat -C -b kafka-server -G ${KAFKACAT_CONSUMER_GROUP} ${DEVICE_DATA_TOPIC} -f '%k:%s\n' 
-        # | kafkacat -P -b kafka-server -t abcde -K:  
+
+        echo "Starting loopback ...."
+        kafkacat -C -b ${KAFKA_HOST} -q -f '{ "key": "%k" , "msg": %s }\n' -u -G ${LOOPBACK_CONSUMER_GROUP} ${DEVICE_DATA_TOPIC} | \
+        unbuffer -p jq -r '"\(.key)@{\"event\": \"configure\",\"meta\": {\"service\": \"\(.msg.metadata.tenant)\",\"timestamp\": \(.msg.metadata.timestamp)},\"data\" : {\"id\" : \"\(.msg.metadata.deviceid)\",\"attrs\": \(.msg.attrs)}}"' \
+        |  kafkacat -P -b ${KAFKA_HOST} -t ${DEVICE_CONFIGURE_TOPIC} -K @ -l
+        
+        echo "Application Stopped restarting ..."
     else
         "Restarting - unable to retrieve '${DEVICE_DATA}' and '${DEVICE_CONFIGURE}' topics"
     fi
@@ -41,4 +45,3 @@ else
    echo "Restarting token not available ..."
     exit 1
 fi
-
